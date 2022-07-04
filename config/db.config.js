a30dcf98d2fc96bfb12654dfbf9b172eb9d72086
Tusharkanti <tusharkantijoshi@gmail.com>
2022-07-04 12:55:40 +0530

    Checkpoint 5

--- config/db.config.js
+++ config/db.config.js
@@ -1,14 +1,3 @@
-var mongoClient = require("mongodb").MongoClient;
-
-var dbUrl = "mongodb://localhost:27017/moviebooking";
-
-var client = new mongoClient(dbUrl, { useUnifiedTopology: true });
-
-client.connect((err, dbInstance) => {
-    if (err) {
-        console.log("Connection Failed :", err);
-    } else {
-        console.log("Connection Established");
-        let db = dbInstance.db("moviebooking");
-    }
-}); 
+module.exports = {
+    url: "mongodb://localhost:27017/moviebooking",
+} 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const port = 9000;
 const http = require("http");
 const httpStatus = require("http-status-codes");
+const db = require("./models/index");
 
 const routeResponseMap = {
   "/movies": "All Movies Data in JSON format from Mongo DB",
@@ -25,4 +26,17 @@ const app = http.createServer((req, res) => {
   }
 });
 
-app.listen(port); 
+app.listen(port);
+
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
